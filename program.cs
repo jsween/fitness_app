a3b71f6c42f63e23d91bade7b5063eed3c690ac9
Jon <jonsweeney26@gmail.com>
2016-05-04 14:12:27 -0700

    init commit

---
+++ program.cs
@@ -0,0 +1,14 @@
+class Program
+{
+  static void Main()
+  {
+    // Prompt user for mins exercised
+    System.Console.Write("Enter how many minutes you exercised: ");
+    string entry = System.Console.ReadLine();
+
+    //add mins
+
+    //display
+    //repeat
+  }
+}
